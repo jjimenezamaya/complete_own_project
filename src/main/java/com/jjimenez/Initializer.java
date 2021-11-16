b17f36affbf2562822352998a29fa19aea4e6143
jjimenez <j.jimenez@indizen.com>
2021-11-16 16:37:56 +0100

    Probando

--- src/main/java/com/jjimenez/Initializer.java
+++ src/main/java/com/jjimenez/Initializer.java
@@ -1,2 +1,9 @@
-package com.jjimenez;public class Initializer {
+package com.jjimenez;
+
+public class Initializer {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
 }
